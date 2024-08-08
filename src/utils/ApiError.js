bde83e63f84292fe3ed2fe222db79e139b1bc8f0
HassanAbbaccy <hassanabbaccy@gmail.com>
2024-08-08 14:36:50 +0500

    cloudinary and multer midleware files

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -5,7 +5,7 @@ class ApiError extends Error{
         statusCode,
         message= "Something went Wrong",
         errors = [],
-        statck = ""
+        stack = ""
 
     ){       //constructor Overriding   
         super(message)
@@ -15,8 +15,8 @@ class ApiError extends Error{
         this.success = false;
         this.errors = errors
 
-        if(statck){
-            this.stack = statck
+        if(stack){
+            this.stack = stack
         }else{
             Error.captureStackTrace(this, this.constructor)
         }
