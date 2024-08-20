41daedc99d4988d9ae4ff96bbb87fc4512ffc02d
HassanAbbaccy <hassanabbaccy@gmail.com>
2024-08-20 10:55:00 +0500

    user router asyn handler and user controllers

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 //function for connection handler
 
 const asyncHanlder = (requestHandler) => {
-  (req, res, next) => {
+ return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).
     catch((err) => next(err));
   };
